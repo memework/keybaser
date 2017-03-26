9b77994a9e208c8b777e5928bd6a9ecddd477334
Luna Mendes <lkmnds@gmail.com>
2017-03-26 02:40:52 -0300

    add kbutils.py

---
+++ kbutils.py
@@ -0,0 +1,60 @@
+import aiohttp
+import asyncio
+import json
+import urllib
+import logging
+
+KB_API_URL = 'https://keybase.io/_/api/1.0/%s.json'
+
+KB_LOOKUP_URL = KB_API_URL % 'lookup'
+
+loop = asyncio.get_event_loop()
+logger = logging.getLogger('kbutils')
+
+# define custom errors
+class JSONError(Exception):
+    pass
+
+class AiohttpError(Exception):
+    pass
+
+class KeybaseError(Exception):
+    pass
+
+async def json_load(string):
+    try:
+        future_json = loop.run_in_executor(None, json.loads, string)
+        return (await future_json)
+    except Exception as err:
+        raise JSONError("Error parsing JSON data")
+
+async def http_get(self, url, **kwargs):
+    timeout = kwargs.get('timeout', 5)
+
+    try:
+        response = await asyncio.wait_for(aiohttp.request('GET', url), timeout)
+        content = await response.text()
+        return content
+    except Exception as err:
+        logger.error('http_get', exc_info=True)
+        raise err
+
+async def keybase_request(self, url, **kwargs):
+    content = await self.http_get(url, **kwargs)
+    data = await self.json_load(content)
+
+    # check the data for errors
+    status = data['status']
+    stcode = status['code']
+
+    if stcode != 0:
+        # API gave up on us
+        raise KeybaseError('{}: {}'.format(status['name'], status['desc']))
+
+    return data
+
+async def kblookup(lookup_string, lookup_type='usernames'):
+    querystr = urllib.urlencode((lookup_type, lookup_string))
+    url = f'{KB_LOOKUP_URL}?{querystr}'
+    data = await keybase_request(url)
+    return data
--- keybaser.py
+++ keybaser.py
@@ -1,9 +1,16 @@
 import time
+import json
+import aiohttp
+import traceback
+import asyncio
 import discord
+import kbutils as utils
+import logging
+
 from discord.ext import commands
 import config as kb_config
 
-bot = commands.Bot(command_prefix=['kb!', 'Kb!', 'kB!'])
+bot = commands.Bot(command_prefix=['kb!', 'Kb!', 'kB!', 'KB!'])
 
 @bot.event
 async def on_ready():
@@ -22,8 +29,21 @@ async def ping(ctx):
     delta = (tfinish - tinit) * 1000
     await bot.edit_message(m, '`%.2fms`' % (delta))
 
+@bot.command(pass_context=True)
+async def lookup(ctx, user : str, location : str = ''):
+    if len(location.strip()) < 1:
+        location = None
+    res = await utils.kblookup(user, location)
+    await bot.say("We got %d bytes from API" % len(res))
+
+async def _say(m, string):
+    await bot.send_message(m.channel, string)
+
 @bot.event
 async def on_message(message):
-    await bot.process_commands(message)
+    try:
+        await bot.process_commands(message)
+    except Exception as err:
+        await _say(message, "fuck, we got error: ```%s```" % (traceback.format_exc()))
 
 bot.run(kb_config.token)
