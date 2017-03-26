3afa70800a71143a6340d756dfe01159c77e605d
Luna Mendes <lkmnds@gmail.com>
2017-03-26 02:05:21 -0300

    use commands.Bot

--- keybaser.py
+++ keybaser.py
@@ -1,30 +1,29 @@
 import time
 import discord
+from discord.ext import commands
 import config as kb_config
 
-client = discord.Client()
+bot = commands.Bot(command_prefix=['kb!', 'Kb!', 'kB!'])
 
-PREFIX = 'kb!'
-
-@client.event
-async def on_message(message):
-    if message.content.lower().startswith(PREFIX):
-        cmd = message.content[3:]
-
-        print(f"recv cmd {cmd}")
-        if cmd == 'ping':
-            tinit = time.monotonic()
-            m = await client.send_message(message.channel, 'pong')
-            tfinish = time.monotonic()
-
-            delta = (tfinish - tinit) * 1000
-            await client.edit_message(m, '%.2fms' % (delta))
-
-@client.event
+@bot.event
 async def on_ready():
-    print('--READY--')
-    print(client.user.name)
-    print(client.user.id)
-    print('!!!!!')
+    print('!!READY!!')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
 
-client.run(kb_config.token)
+@bot.command(pass_context=True)
+async def ping(ctx):
+    """Ping the fucker."""
+    tinit = time.monotonic()
+    m = await bot.say('pong')
+    tfinish = time.monotonic()
+
+    delta = (tfinish - tinit) * 1000
+    await bot.edit_message(m, '`%.2fms`' % (delta))
+
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+
+bot.run(kb_config.token)
